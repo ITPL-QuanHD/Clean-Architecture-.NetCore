0a0e1b983124a3c9960a7bfdc1f1a2c73e72613a
QuanHD0610 <quanhd@itp-link.com>
2025-03-10 08:12:14 +0700

    update IRepository

--- Member.Application/IRepository/IMemberRepository.cs
+++ Member.Application/IRepository/IMemberRepository.cs
@@ -1,13 +1,14 @@
-﻿using Member.Domain.Entities;
+﻿using Member.Application.DTOs;
+
 
 namespace Member.Application.IRepository
 {
     public interface IMemberRepository
     {
-        Task<List<Members>> GetAllMembersAsync();
-        Task<Members> GetMemberByIdAsync(int id);
-        Task<bool> AddMemberAsync(Members member);
-        Task<bool> UpdateMemberAsync(Members member);
+        Task<List<MemberDto>> GetAllMembersAsync();
+        Task<MemberDto> GetMemberByIdAsync(int id);
+        Task<bool> AddMemberAsync(MemberDto member);
+        Task<bool> UpdateMemberAsync(MemberDto member);
         Task<bool> DeleteMemberAsync(int id);
     }
 }
